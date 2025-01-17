6efb99d1c18a1975ffa116ceea7eb5ab56c43731
cimv <cimcrd@umsystem.edu>
2025-01-17 17:42:20 -0600

    starting test func

--- src/tests.py
+++ src/tests.py
@@ -4,16 +4,18 @@ import torch
 import argparse
 
 def test(model,dataloader):
-	pass
+
+	model.eval()
 
 	#ITERATE THROUGH DATA
 	for X,T in dataloader:
 		X = X.to(cuda_device,non_blocking=True)
 		T = T.to(cuda_device,non_blocking=True)
 
-		with torch.set_grad_enabled(True):
+		with torch.set_grad_enabled(False):
 			output = model(X)
-			loss   = loss_fn(output,T)
+			_,Y    = torch.max(output,1)
+
 
 if __name__ == '__main__':
 	pass
