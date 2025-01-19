55edd7ba10715e0288fbcdf31844a3601d6381dd
cimv <cimcrd@umsystem.edu>
2025-01-18 23:45:03 -0600

    starting on kjobs

--- src/kjobs.py
+++ src/kjobs.py
@@ -1,11 +1,24 @@
 import pyyaml
 import subprocess
+import argparse
+
 ####################################################################################################
-N_JOBS = 25
-####################################################################################################
-#load parameters
+parser = argparse.ArgumentParser()
+parser.add_argument('--params',default=None,help='Path to json file listing hyperparameters.')
 
 #launch via kubectl
+def launch
 for job in range(N_JOBS):
 	pass
-	kubectl create -f job_template.yml
+	subprocess."kubectl create -f job_train.yml"
+
+
+if __name__ == '__main__':
+
+	args = parser.parse_args()
+
+	if args.params is None:
+		print("No .json given for hyperparameter list.")
+		sys.exit(1)
+
+	json_path = args.params
