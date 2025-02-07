403cbe26dd9898e3db80ad8dd170822020b9cf48
cimv <cimcrd@umsystem.edu>
2025-02-06 23:58:59 -0600

    hierarchical choice of tiles?

--- src/split.py
+++ src/split.py
@@ -17,4 +17,23 @@ if __name__ == '__main__':
     np.random.seed(476)
     random.seed(476)
 
-    
+	all_rasters = [i[0:-19] for i in band_files] #19456
+	all_tiles = [i[-25:-19] for i in band_files] #19456
+	
+	unique_r,r_inv,r_cnt = np.unique(all_rasters,return_inverse=True,return_counts=True) #726
+	tiles   = [i[-6:] for i in unique_r] #726
+	unique_t,t_inv,t_cnt = np.unique(tiles,return_inverse=True,return_counts=True) #21
+
+	# CHOOSE 4 TILES AT RANDOM
+	tile_choices     = np.random.choice(unique_t,4,replace=False)
+	validation_tiles = tile_choices[0:2]
+	test_tiles       = tile_choices[2:]
+
+	validation_mask  = np.isin(all_tiles,validation_tiles)
+	validation_idx   = np.where(validation_mask)[0]
+	test_mask        = np.isin(all_tiles,test_tiles)
+	test_idx         = np.where(test_mask)[0]
+	training_mask    = ~(validation_mask+test_mask)
+	training_idx     = np.where(training_mask)[0]
+	
+	return training_idx,validation_idx,test_idx
