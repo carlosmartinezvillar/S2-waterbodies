ca28ddcd734e6d470200e11ba109247df43e2ac0
cimv <cimcrd@umsystem.edu>
2025-03-29 21:46:53 -0500

    added checks for deleting subsets of jobs with --start and --end

--- src/kjobs.py
+++ src/kjobs.py
@@ -1,5 +1,5 @@
 import yaml
-import subprocess
+import subprocess as sp
 import argparse
 import os
 import json
@@ -9,9 +9,9 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument('--spec',default=None,required=False,help='Path to original YAML to base calls off')
 parser.add_argument('--params',default=None,required=False,help='Path to json file listing hyperparameters.')
-parser.add_argument('--start',type=int,default=0,help='Starting row (zero indexed)')
-parser.add_argument('--end',type=int,default=1,help='End row zero-indexed using [start,end)')
-parser.add_argument('--clear',required=False,action='store_true',help='Delete all jobs')
+parser.add_argument('--start',type=int,default=None,help='Starting row (zero indexed)')
+parser.add_argument('--end',type=int,default=None,help='End row zero-indexed using [0,end)')
+parser.add_argument('--clear',required=False,action='store_true',help='Delete jobs')
 args = parser.parse_args()
 
 #launch via kubectl
@@ -46,7 +46,7 @@ def launch_jobs(template_path,start,end,hp_list):
 			yaml.dump(obj,fp_temp)
 
 		#run
-		out = subprocess.run("kubectl create -f ../cfg/temp.yml",capture_output=True,text=True,shell=True)
+		out = sp.run("kubectl create -f ../cfg/temp.yml",capture_output=True,text=True,shell=True)
 		if out.returncode != 0:
 			print(f"ERROR launching job for model listed in row {row}")
 			print(out.stderr)
@@ -56,34 +56,57 @@ def launch_jobs(template_path,start,end,hp_list):
 	os.remove('../cfg/temp.yml')
 
 
-def clear_jobs():
+def clear_jobs(start,end):
 	'''
 	Get the list of jobs from the cluster and remove them.
 	'''
-	out   = subprocess.run(f"kubectl get jobs",capture_output=True,text=True,shell=True)
+	out   = sp.run(f"kubectl get jobs",capture_output=True,text=True,shell=True)
 	lines = out.stdout.split('\n')
-	for line in lines[1:-1]:
-		job_str = line.split()[0]
-		del_out = subprocess.run(f"kubectl delete job {job_str}",capture_output=True,text=True,shell=True)
-		print(del_out.stdout)
+	jobs  = [line.split()[0] for line in lines[1:-1]]
+	jobnr = [int(job.split('-')[-1]) for job in jobs]
+
+	if (start is None) and (end is None): #delete all
+		for job in jobs:
+			del_out = sp.run(f"kubectl delete job {job}",capture_output=True,text=True,shell=True)
+			print(del_out.stdout)
+	
+	if (start is None) and (end is not None): #delete (0,end]
+		for i in jobnr[:jobnr.index(end)+1]:
+			del_out = sp.run(f"kubectl delete job train-job-{i}",capture_output=True,text=True,shell=True)
+			print(del_out.stdout)
+
+	if (start is not None) and (end is None): #delete [start,N)
+		for i in jobnr[jobnr.index(start):]:
+			del_out = sp.run(f"kubectl delete job train-job-{i}",capture_output=True,text=True,shell=True)
+			print(del_out.stdout)
+
+	if (start is not None) and (end is not None): #delete [start,end]
+		for i in jobnr[jobnr.index(start):jobnr.index(end)+1]:
+			del_out = sp.run(f"kubectl delete job train-job-{i}",capture_output=True,text=True,shell=True)
+			print(del_out.stdout)
 
 if __name__ == '__main__':
-	if args.clear: #do this and nothing else.
-		print("CLEARING ALL JOBS...")
-		clear_jobs()
+	if args.clear is True: #do this and nothing else.
+		print("CLEARING JOBS...")
+		clear_jobs(args.start,args.end)
 		sys.exit(1)
 
+	#CHECK YAML TEMPLATE EXISTS
+	assert os.path.isfile(args.spec), "(kjobs.py): INCORRECT YAML TEMPLATE PATH IN SPEC ARG"
+
 	#CHECK+LOAD HYPERPARAMETER FILE
-	assert args.params is not None, "No .json given for hyperparameter list."
-	assert os.path.isfile(args.params), "kjobs.py: INCORRECT JSON FILE PATH"
+	assert args.params is not None, "(kjobs.py): No JSON given."
+	assert os.path.isfile(args.params), "(kjobs.py): INCORRECT JSON FILE PATH"
 	with open(args.params,'r') as fp:
 		HP_LIST = [json.loads(l) for l in fp.readlines()]
 	N = len(HP_LIST)
 	assert N > 0, "kjobs.py: GOT EMPTY JSON FILE."
 
 	#CHECK START+END ARGS
-	assert args.end <= N, "kjobs.py: END INDEX IN ARGS OUT OF RANGE"
-	assert args.end > args.start, "kjobs.py: INCORRECT INDICES IN ARGS"
+	assert args.start is not None, "(kjobs.py): GOT NONE FOR START ROW"
+	assert args.end is not None, "(kjobs.py): GOT NONE FOR END ROW"
+	assert args.end <= N, "(kjobs.py): END INDEX OUT OF RANGE"
+	assert args.end > args.start, "(kjobs.py): END ROW > START ROW"
 
 	#SET
 	start_index = args.start
@@ -92,8 +115,6 @@ if __name__ == '__main__':
 	else:
 		end_index = N #for range(start,end) which already n-1
 
-	#CHECK YAML TEMPLATE EXISTS
-	assert os.path.isfile(args.spec), "kjobs.py: INCORRECT YAML TEMPLATE PATH IN SPEC ARG"
 
 	#RUN
 	launch_jobs(args.spec,start_index,end_index,HP_LIST)
