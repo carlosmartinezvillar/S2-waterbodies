57a655a4d8b4d7e7db1cafb1880d9331e26cf4e1
cimv <cimcrd@umsystem.edu>
2026-03-25 13:57:14 -0700

    added dataloaders for three benchmark datasets

--- src/dload.py
+++ src/dload.py
@@ -100,18 +100,45 @@ class SentinelDataset(torch.utils.data.Dataset):
 
 
 class PotsdamDataset(torch.utils.data.Dataset):
-	def __init__(self,chip_dir,n_labels,train,transform=None):
+	'''
+	512x512
+	0:void,1:impervious,2:building,3:low veg,4:tree,5:car,6:clutter/backg #6CLASS
+	0:void+clutter,1:impervious,2:building,3:low veg,4:tree,5:car #5CLASS
+	IGNORE 0 in LOSS ! 
+	'''
+	def __init__(self,chip_dir,train,split,validate,transform=None):
 		self.root_dir = chip_dir
 
 		if train == True:
-			self.img_dir = "./img_dir/train/"
-			self.ann_dir = "./ann_dir/train/"
-		else:
-			self.img_dir = "./img_dir/val/"
-			self.ann_dir = "./ann_dir/val/"
+			#For both
+			self.img_dir = f"{self.root_dir}/img_dir/train/"
+			self.ann_dir = f"{self.root_dir}/ann_dir/train/"
 
-		self.img_paths = sorted(glob.glob(f"{self.img_dir}*.png"))
-		self.ann_paths = sorted(glob.glob(f"{self.ann_dir}*.png"))
+			#LEAVE OUT TILES
+			if split == True: 
+				vtiles = ['2_10','7_9']
+				if validate == True:
+					self.img_paths = []
+					self.ann_paths = []
+					for tile in vtiles:
+						self.img_paths.append(sorted(glob.glob(f"{self.img_dir}{tile}*.png")))
+						self.ann_paths.append(sorted(glob.glob(f"{self.ann_dir}{tile}*.png")))
+				else:
+					all_ids = glob.glob("*.png",root_dir=self.img_dir) #ann and img match	
+					filtered = [f for f in all_ids if not os.path.basename(f).startswith(tuple(vtiles))]
+					self.img_paths = [f"{self.img_dir}{f}" for f in filtered]
+					self.ann_paths = [f"{self.ann_dir}{f}" for f in filtered]
+
+			#ENTIRE TRAIN FOLDER
+			else: 
+				self.img_paths = sorted(glob.glob(f"{self.img_dir}*.png"))
+				self.ann_paths = sorted(glob.glob(f"{self.ann_dir}*.png"))
+
+		else:
+			self.img_dir = f"{self.root_dir}/img_dir/val/"
+			self.ann_dir = f"{self.root_dir}/ann_dir/val/"
+			self.img_paths = sorted(glob.glob(f"{self.img_dir}*.png"))
+			self.ann_paths = sorted(glob.glob(f"{self.ann_dir}*.png"))
 
 		self.img_transform = v2.Compose([
 			v2.ToImage(),
@@ -122,45 +149,138 @@ class PotsdamDataset(torch.utils.data.Dataset):
 			v2.ToImage(),
 			v2.ToDtype(torch.int64)
 		])
-		pass
+
+		self.additional_transform = transform
+
 
 	def __len__(self):
-		return len(self.ids)
+		return len(self.ann_paths)
+
 
 	def __getitem__(self,idx):
-		rgb = Image.open(f"{self.img_paths[idx]}.png")
-		lbl = Image.open(f"{self.ann_paths[idx]}.png")
-		image = self.img_transform(rgb)
-		label = self.ann_transform(lbl)
+		image = self.img_transform(Image.open(f"{self.img_paths[idx]}"))
+		label = self.ann_transform(Image.open(f"{self.ann_paths[idx]}"))
+		if self.additional_transform:
+			image,label = self.additional_transform(image,label)
 		return image,label
 
 
 class LoveDADataset(torch.utils.data.Dataset):
-	def __init__(self,root_dir):
-		pass
+	# 1024x1024
+	# No-Data     0): 255, 255, 255 (White) — IGNORE IN LOSS!
+	# Background  1): 255, 0, 0 (Red)
+	# Building    2): 255, 255, 0 (Yellow)
+	# Road        3): 128, 0, 128 (Purple)
+	# Water       4): 0, 0, 255 (Blue)
+	# Barren      5): 255, 165, 0 (Orange)
+	# Forest      6): 0, 128, 0 (Green)
+	# Agriculture 7): 0, 255, 0 (Lime)
+	def __init__(self,chip_dir,train,transform=None):
+		self.root_dir = chip_dir
+
+		if train == True: # TEST SET IS ONLINE
+			self.img_dir = f"{self.root_dir}/img_dir/train/"
+			self.ann_dir = f"{self.root_dir}/ann_dir/train/"
+			self.img_paths = sorted(glob.glob(f"{self.img_dir}*.png"))
+			self.ann_paths = sorted(glob.glob(f"{self.ann_dir}*.png"))
+		else:
+			self.img_dir = f"{self.root_dir}/img_dir/val/"
+			self.ann_dir = f"{self.root_dir}/ann_dir/val/"
+			self.img_paths = sorted(glob.glob(f"{self.img_dir}*.png"))
+			self.ann_paths = sorted(glob.glob(f"{self.ann_dir}*.png"))
+
+		self.img_transform = v2.Compose([
+			v2.ToImage(),
+			v2.ToDtype(torch.float32,scale=True)
+		])
+
+		self.ann_transform = v2.Compose([
+			v2.ToImage(),
+			v2.ToDtype(torch.int64)
+		])
+
+		self.additional_transform = transform
+
 
 	def __len__(self):
-		pass
-
-	def __getitem__(self,i):
-		pass
-		return
+		return len(self.ann_paths)
 
 
-class iSaidDataset(torch.utils.data.Dataset):
-	def __init__(self,root_dir):
-		pass
+	def __getitem__(self,idx):
+		image = self.img_transform(Image.open(f"{self.img_paths[idx]}"))
+		label = self.ann_transform(Image.open(f"{self.ann_paths[idx]}"))
+		if self.additional_transform:
+			image,label = self.additional_transform(image,label)
+		return image,label
+
+
+class iSAIDDataset(torch.utils.data.Dataset):
+	def __init__(self,chip_dir,train,split,validate,transform=None,):
+		'''
+		896x896 with 512 overlap
+		Background          0): 0, 0, 0  <--- IGNORE IN LOSS!
+		Ship                1): 0, 0, 63
+		Store Tank          2): 0, 63, 0
+		Baseball Diamond    3): 0, 63, 127
+		Tennis Court        4): 0, 63, 191
+		Basketball Court    5): 0, 63, 255
+		Ground Track Field  6): 0, 127, 0
+		Bridge              7): 0, 127, 63
+		Large Vehicle       8): 0, 127, 127
+		Small Vehicle       9): 0, 127, 191
+		Helicopter         10): 0, 127, 255
+		Swimming Pool      11): 0, 191, 0
+		Roundabout         12): 0, 191, 63
+		Soccer Ball Field  13): 0, 191, 127
+		Plane              14): 0, 191, 191
+		Harbor             15): 0, 191, 255
+		'''
+		self.root_dir = chip_dir
+
+		if train == True:
+			#For both
+			self.img_dir = f"{self.root_dir}/img_dir/train/"
+			self.ann_dir = f"{self.root_dir}/ann_dir/train/"
+
+			#LEAVE OUT TILES
+			if split == True: 
+				vtiles = ['P0003','P0011','P0002']
+				if validate == True:
+					self.img_paths = []
+					self.ann_paths = []
+					for tile in vtiles:
+						self.img_paths.append(sorted(glob.glob(f"{self.img_dir}{tile}_*.png")))
+						self.ann_paths.append(sorted(glob.glob(f"{self.ann_dir}{tile}_*.png")))
+				else:
+					all_ids = glob.glob("*.png",root_dir=self.img_dir) #ann and img match	
+					filtered = [f for f in all_ids if not os.path.basename(f).startswith(tuple(vtiles))]
+					self.img_paths = [f"{self.img_dir}{f}" for f in filtered]
+					self.ann_paths = [f"{self.ann_dir}{f}" for f in filtered]
+
+			#ENTIRE TRAIN FOLDER
+			else: 
+				self.img_paths = sorted(glob.glob(f"{self.img_dir}*.png"))
+				self.ann_paths = sorted(glob.glob(f"{self.ann_dir}*.png"))			
+
+
+		else:
+			self.img_dir = f"{self.root_dir}/img_dir/val/"
+			self.ann_dir = f"{self.root_dir}/ann_dir/val/"
+			self.img_paths = sorted(glob.glob(f"{self.img_dir}*.png"))
+			self.ann_paths = sorted(glob.glob(f"{self.ann_dir}*.png"))
 
 	def __len__(self):
-		pass
+		return len(self.ann_paths)
 
-	def __getitem__(self,i):
-		pass
-		return
-
+	def __getitem__(self,idx):
+		image = self.img_transform(Image.open(f"{self.img_paths[idx]}"))
+		label = self.ann_transform(Image.open(f"{self.ann_paths[idx]}"))
+		if self.additional_transform:
+			image,label = self.additional_transform(image,label)
+		return image,label
 
 ################################################################################
-# EXTERNAL DATASETS PREPROCESSING
+# EXTERNAL DATASETS PREPROCESSING -- If further pre-process needed
 ################################################################################
 def preprocess_potsdam():
 	CHP_SIZE = 512
@@ -168,10 +288,12 @@ def preprocess_potsdam():
 
 
 def preprocess_loveda():
+	CHP_SIZE = 1024
 	pass
 
 
 def preprocess_isaid():
+	CHP_SIZE = 896
 	pass
 
 
@@ -181,6 +303,8 @@ def preprocess_isaid():
 if __name__ == '__main__':
 
 	__spec__ = None #multiprocessing w/ interactive/debugger
+
+	from tqdm import tqdm
 
 	print('-> dload.py')
 
@@ -198,53 +322,109 @@ if __name__ == '__main__':
 		v2.RandomVerticalFlip(p=0.5)
 	])
 
-	tr_ds = SentinelDataset(f"{DATA_DIR}/training",
-		n_bands=3,
-		n_labels=2,
-		transform=transform)
+	############################################################
+	# S2-DW
+	############################################################
+	# s2dw_tr_ds = SentinelDataset(f"{DATA_DIR}/training",
+	# 	n_bands=3,
+	# 	n_labels=2,
+	# 	transform=transform)
 
-	va_ds = SentinelDataset(f"{DATA_DIR}/validation",
-		n_bands=3,
-		n_labels=2,
-		transform=None)
+	# s2dw_va_ds = SentinelDataset(f"{DATA_DIR}/validation",
+	# 	n_bands=3,
+	# 	n_labels=2,
+	# 	transform=None)
+	# dataloaders = {
+	# 	'training': torch.utils.data.DataLoader(
+	# 		s2dw_tr_ds,
+	# 		batch_size=8,
+	# 		drop_last=False,
+	# 		shuffle=True,
+	# 		num_workers=3),
+	# 	'validation': torch.utils.data.DataLoader(
+	# 		s2dw_va_ds,
+	# 		batch_size=8,
+	# 		drop_last=False,
+	# 		shuffle=False,
+	# 		num_workers=0)
+	# }
+	# print("\nS2-DW -- TESTING BATCH ITERATION")
 
+	############################################################
+	# POTSDAM
+	############################################################
+	# potsdam_tr_ds = PotsdamDataset(f"{DATA_DIR}",train=True,split=True,validate=False,transform=transform)
+	# potsdam_va_ds = PotsdamDataset(f"{DATA_DIR}",train=True,split=True,validate=True)
+	# dataloaders = {
+	# 	'training': torch.utils.data.DataLoader(
+	# 		potsdam_tr_ds,
+	# 		batch_size=8,
+	# 		drop_last=False,
+	# 		shuffle=True,
+	# 		num_workers=3),
+	# 	'validation': torch.utils.data.DataLoader(
+	# 		potsdam_va_ds,
+	# 		batch_size=8,
+	# 		drop_last=False,
+	# 		shuffle=False,
+	# 		num_workers=0)
+	# }
+	# print("\nPOTSDAM -- TESTING BATCH ITERATION")
+
+
+	############################################################
+	# LOVEDA
+	############################################################
+	# loveda_tr_ds = LoveDADataset(f"{DATA_DIR}",train=True,transform=transform)
+	# loveda_va_ds = LoveDADataset(f"{DATA_DIR}",train=False)
+	# dataloaders = {
+	# 	'training': torch.utils.data.DataLoader(
+	# 		loveda_tr_ds,
+	# 		batch_size=8,
+	# 		drop_last=False,
+	# 		shuffle=True,
+	# 		num_workers=3),
+	# 	'validation': torch.utils.data.DataLoader(
+	# 		loveda_va_ds,
+	# 		batch_size=8,
+	# 		drop_last=False,
+	# 		shuffle=False,
+	# 		num_workers=0)
+	# }
+	# print("\nLOVEDA -- TESTING BATCH ITERATION")
+
+
+	############################################################
+	# iSAID
+	############################################################
+	isaid_tr_ds = iSAIDDataset(f"{DATA_DIR}",train=True,split=True,validate=False,transform=transform)
+	isaid_va_ds = iSAIDDataset(f"{DATA_DIR}",train=True,split=True,validate=True)
 	dataloaders = {
 		'training': torch.utils.data.DataLoader(
-			tr_ds,
+			isaid_tr_ds,
 			batch_size=8,
 			drop_last=False,
 			shuffle=True,
 			num_workers=3),
 		'validation': torch.utils.data.DataLoader(
-			va_ds,
+			isaid_va_ds,
 			batch_size=8,
 			drop_last=False,
 			shuffle=False,
 			num_workers=0)
 	}
+	print("\niSAID -- TESTING BATCH ITERATION")
 
-
-	print("\nTESTING BATCH ITERATION")
-	print("-"*40)
-	for epoch in range(5):
+	################
+	# ITERATE
+	################
+	for epoch in range(2):
 		print(f"\nepoch nr. {epoch}")
 		print("-"*20)
+		t = tqdm(total=len(dataloaders['training']),ncols=80,ascii=True)
 		for i, (X,T) in enumerate(dataloaders['training']):
-		# for i,(X,T,img_path) in enumerate(dataloaders['training']):
-			# if i==0:
-				# print("First batch")
-				# for p in img_path:print(p)
 			pass
+			t.update(1)
+		t.close()
 
-
-	# print("-"*40)
-	# print(f"CHECKING {tr_ds.__class__.__name__}")
-	# x,t = tr_ds[0]
-	# print(f'SHAPE: {x.shape} {x.dtype}\n{t.shape} {t.dtype}')
-	# print("-"*40)	
-	# start = time.time()
-	# for x,t in ds:
-	# 	pass
-	# delta = time.time() - start
-	# print(f"TOTAL TIME: {delta:.5f} | SAMPLES: {len(ds)}")
 	pass
